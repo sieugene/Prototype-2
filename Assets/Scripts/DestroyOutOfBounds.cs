3b6ad577649c69e3b18ab3fe8ad34f47751d9dc7
sieugene <sieugene@mail.ru>
2025-02-17 17:38:17 +0900

    feat: add lower bound for object destruction in DestroyOutOfBounds script

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     public float topBound = 30;
+    public float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,7 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topBound)
+        if (transform.position.z > topBound || transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }

87f3bd9f8d1e1916b6de0157149821ae0fce1fe4
sieugene <sieugene@mail.ru>
2025-02-19 19:36:57 +0900

    feat: implement SpawnManager to spawn animals on key press

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPozZ = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPoz = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPozZ);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPoz, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
